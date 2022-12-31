17b4961dd4da13e742f882560fa63e4a786473e1
Ahmed Shalaby <kingshalaby@yahoo.com>
2022-12-31 21:48:12 +0200

    stop words

--- analyzer.rb
+++ analyzer.rb
@@ -1,17 +1,31 @@
+stopwords = %w{the a by on for of are with just but and to the my I has
+some in}
 lines = File.readlines("text.txt")
 line_count = lines.size
 text = lines.join
+# Count the words, characters, paragraphs and sentences
 word_count = text.split.length
 character_count = text.length
 character_count_nospaces = text.gsub(/\s+/, '').length
 paragraph_count = text.split(/\n\n/).length
 sentence_count = text.split(/\.|\?|!/).length
+
+# Make a list of words in the text that aren't stop words,
+# count them, and work out the percentage of non-stop words
+# against all words
+all_words = text.scan(/\w+/)
+good_words = all_words.reject{ |word| stopwords.include?(word) }
+good_percentage = ((good_words.length.to_f / all_words.length.to_f) * 100).
+  to_i
+# Give the analysis back to the user
 puts "#{line_count} lines"
 puts "#{character_count} characters"
-puts "#{character_count_nospaces} characters excluding spaces"
+
+puts "#{character_count_nospaces} characters (excluding spaces)"
 puts "#{word_count} words"
-puts "#{paragraph_count} paragraphs"
 puts "#{sentence_count} sentences"
+puts "#{paragraph_count} paragraphs"
 puts "#{sentence_count / paragraph_count} sentences per paragraph
 (average)"
 puts "#{word_count / sentence_count} words per sentence (average)"
+puts "#{good_percentage}% of words are non-fluff words"
